599e3f0f941dbfbdf8a9777ac464454680048f25
Maël Segalen <segalen.mael@gmail.com>
2019-04-21 19:32:10 +0200

    First commit
    First commit + basic folders + first draft

---
+++ Assets/KinectView/Scripts/DisableOnStart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+
+public class DisableOnStart : MonoBehaviour {
+ 
+    // Use this for initialization
+    void Start () 
+    {
+        gameObject.SetActive (false);
+}
+}
